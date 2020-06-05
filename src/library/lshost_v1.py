8c089638540c9a24c4e9618e762f182e07c2eb90
Ayush Jain <aj4ayushjain@gmail.com>
2020-06-05 16:37:25 +0530

    Update lshost_v1

--- src/library/lshost_v1.py
+++ src/library/lshost_v1.py
@@ -2,7 +2,6 @@
 
 from ansible.module_utils.basic import *
 from ansible.module_utils.urls import *
-import urllib3
 def spectrum_auth(module,username,password,config_ip):
 	
 	headers = {
@@ -13,7 +12,7 @@ def spectrum_auth(module,username,password,config_ip):
 	url = 'https://{0}/rest/auth'.format(config_ip)
 	
 	try:
-		response = fetch_url (module, url, headers=headers,method ='POST',verify_certs=False)
+		response = open_url (url, headers=headers,method ='POST',validate_certs=False)
 		data = json.loads(response.read())
 		if data['status']['message']=='Authentication failed':
 			return 1,None, "Authentication failed"	
@@ -35,12 +34,11 @@ def main():
 	username = module.params['api_username']		
 	password = module.params['api_password']
 	config_ip = module.params['storage_config_ip']
-	urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 	rc, auth_content, error = spectrum_auth(module, username,password,config_ip)
-	if rc==0:
-		module.exit_json(changed = True, meta = auth_token)
+	if rc==0:	
+		module.exit_json(changed = True, meta = auth_content)
 	else:
-		module.fail_json(msg = 'Error in repo', meta = error)
+		module.fail_json(msg = 'Error in getting authentication token', meta = error)
 
 if __name__ == '__main__':
 	main()
